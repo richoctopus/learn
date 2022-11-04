12ef5226b8ab4f480a017a8bbe62a328cc98a09c
zhangyuyu <zhangyuyu@sjcredit.com>
2022-11-04 11:30:08 +0800

    feat(button): 默认值

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -87,7 +87,7 @@ export default defineComponent({
         ) : (
           ""
         )}
-        {slots.default ? slots.default() : ""}
+        {slots.default ? slots.default() : "按钮"}
       </button>
     );
   },
